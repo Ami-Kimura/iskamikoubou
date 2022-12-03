b7bf322eff82463de94202071c71e54d07320933
Ami-Kimura <unitykmami@gmail.com>
2022-12-03 21:06:41 +0900

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// SP hamburger-menu
 $(".navButton").click(function () {//ボタンクリック
   $(this).toggleClass('active');
    $("#nav-menu").toggleClass('panelactive');
@@ -11,3 +12,40 @@ $(".navButton").click(function () {//ボタンクリック
       $("#nav-menu").removeClass('panelactive');
      $(".circle-menu").removeClass('circleactive');
   });
+
+////////////////////////////////////////////////
+
+
+
+// Smooth Scrool
+$(function() {
+
+  $('a[href^="#"]').click(function() {
+
+    // 初期設定：移動時間(ms)と頭出し位置
+    var speed = 1200;
+    var offset = -150;
+
+    // アンカーを取得
+    var anchor = $(this).attr("href");
+
+    // ターゲットの位置を取得
+    var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+    var position = target.offset().top + offset;
+
+    // スクロール（アニメーション）
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+
+    return false;
+
+  });
+
+});
+
+$('.dummy').click(function() {
+  return false;
+
+});
+////////////////////////////////////////////////
