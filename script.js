6246eedcff94b3b1b4018a0e082e57b4ce318988
Ami-Kimura <unitykmami@gmail.com>
2022-12-07 13:16:45 +0900

    commit

--- script.js
+++ script.js
@@ -80,3 +80,27 @@ $(".sp_reserve").click(function () {
     
 
     });
+
+
+
+
+    function slideSwitch() {
+      var $active = $('#slide img.active');
+   
+      if ( $active.length == 0 ) $active = $('#slide img:last');
+   
+      var $next =  $active.next().length ? $active.next()
+         : $('#slide img:first');
+   
+      $active.addClass('last-active');
+   
+      $next.css({opacity: 0.0})
+         .addClass('active')
+         .animate({opacity: 1.0}, 2000, function() {
+              $active.removeClass('active last-active');
+         });
+   }
+   
+   $(function() {
+      setInterval( "slideSwitch()", 5000 );
+   });
